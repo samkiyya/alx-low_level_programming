11c49d6aa5263227af00c0c21ff32b7d1f501623
samkiyya <samuelkiyya2012@gmail.com>
2023-02-16 05:09:32 -0800

    c task 4-8

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Return: Always 0 (Success)
+*Description: A C program that prints with put function
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
