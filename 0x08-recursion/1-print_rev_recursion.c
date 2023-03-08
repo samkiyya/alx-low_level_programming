da651bf8f86766dfdce65bf9d26ac287630b5393
samkiyya <samuelkiyya2012@gmail.com>
2023-03-08 00:09:33 -0800

    task 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,20 +1,14 @@
 #include "main.h"
-
 /**
- * _puts_recursion - print a string.
- * @s: pointer to char
- * Return: No.
+ *_print_rev_recursion - Print a string in reverse recursively
+ *@s:Pointer to the strint to be printed
+ *Return - void (Always)
  */
-
-void _puts_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
 	if (*s != '\0')
 	{
+		_print_rev_recursion(s + 1);
 		_putchar(*s);
-		_puts_recursion(s + 1);
-	}
-	else
-	{
-	_putchar('\n');
 	}
 }
