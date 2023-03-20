170d20789cde9fd14ca68e56d47dc22a5c9d8109
samkiyya <samuelkiyya2012@gmail.com>
2023-03-20 04:13:31 -0700

    task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,9 +1,17 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include "dog.h"
 
 int _strlen(char *s);
 char *_strcpy(char *dest, char *src);
 
+/**
+ * new_dog - ...
+ * @name: ...
+ * @age: ...
+ * @owner: ...
+ *
+ * Return: ...
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *cutie_dog;
@@ -45,6 +53,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 /**
  * _strlen - Returns the length of a string
+ * @s: String to count
+ *
  * Return: String length
  */
 int _strlen(char *s)
@@ -58,9 +68,14 @@ int _strlen(char *s)
 
 	return (c);
 }
-/* 
- * str copy function
- * */
+
+/**
+ * _strcpy - Copy a string
+ * @dest: Destination value
+ * @src: Source value
+ *
+ * Return: the pointer to dest
+ */
 char *_strcpy(char *dest, char *src)
 {
 	int i;
