240637803748e3797288d9c526c9fda116778c37
samkiyya <samuelkiyya2012@gmail.com>
2023-03-20 04:19:01 -0700

    task 4 updated

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -5,12 +5,12 @@ int _strlen(char *s);
 char *_strcpy(char *dest, char *src);
 
 /**
- * new_dog - 
- * @name: 
- * @age: 
- * @owner: 
+ * new_dog - ...
+ * @name: ...
+ * @age: ...
+ * @owner: ...
  *
- * Return: 
+ * Return: ...
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
