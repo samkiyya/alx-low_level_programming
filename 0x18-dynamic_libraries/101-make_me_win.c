517738f89b8af7664f9aa7a39173a6a39131d68d
samkiyya <samuelkiyya2012@gmail.com>
2023-05-22 12:00:24 -0700

    task 3

---
+++ 0x18-dynamic_libraries/101-make_me_win.c
@@ -0,0 +1,51 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <dlfcn.h>
+
+int wrap_main(int argc, char *argv[]) {
+    // Define the desired winning numbers
+    char *winning_numbers[] = { "9", "8", "10", "24", "75", "9" };
+
+    // Load the original gm program
+    void *handle = dlopen("./gm", RTLD_LAZY);
+    if (!handle) {
+        fprintf(stderr, "Error: Failed to load original gm program\n");
+        return 1;
+    }
+
+    // Get the address of the original main function
+    int (*original_main)(int, char **) = (int (*)(int, char **))dlsym(handle, "main");
+    if (!original_main) {
+        fprintf(stderr, "Error: Failed to find the original main function\n");
+        dlclose(handle);
+        return 1;
+    }
+
+    // Call the original main function with the modified arguments
+    char **new_argv = malloc((argc + 6) * sizeof(char *));
+    if (!new_argv) {
+        fprintf(stderr, "Error: Failed to allocate memory\n");
+        dlclose(handle);
+        return 1;
+    }
+
+    // Copy the original arguments
+    for (int i = 0; i < argc; i++) {
+        new_argv[i] = argv[i];
+    }
+
+    // Add the winning numbers to the arguments
+    for (int i = 0; i < 6; i++) {
+        new_argv[argc + i] = winning_numbers[i];
+    }
+    new_argv[argc + 6] = NULL;
+
+    // Call the original main function with the new arguments
+    int result = original_main(argc + 6, new_argv);
+
+    // Cleanup and exit
+    dlclose(handle);
+    free(new_argv);
+    return result;
+}
--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-wget -P ../ https://github.com/MubarekSD/alx-low_level_programming/raw/main/0x18-dynamic_libraries/libhack.so
-export LD_PRELOAD=../libhack.so
+
+# Wait for 98 seconds
+sleep 98
+
+# Inject the custom shared library using LD_PRELOAD
+export LD_PRELOAD=./101-make_me_win.so
+
+# Run the gm program with the desired numbers
+./gm 9 8 10 24 75 9
